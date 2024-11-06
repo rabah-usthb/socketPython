cb474c38989ef37de963a05387380415046ae04c
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-11-06 03:12:54 +0100

    added comments removed useless while loop in server.py

--- client.py
+++ client.py
@@ -1,35 +1,35 @@
 import socket
 
-host = '192.168.1.70'    # host of server socket
+host = '192.168.1.70'    # host is ip address of server machine
 port = 12800  # port of server socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create TCP client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #create TCP client socket
 data = ""
 client_socket.connect((host, port)) # Connect to the server socket
 print(f"Connected to server at {host}:{port}")
 
-prompt = client_socket.recv(1024).decode('utf-8')
+prompt = client_socket.recv(1024).decode('utf-8')#fetch the password prompt from server
 print(prompt)
 
-user_input = input()
-client_socket.sendall(user_input.encode('utf-8'))
+user_input = input()  #store input of password in user_input
+client_socket.sendall(user_input.encode('utf-8')) #send user_input to server
 
-response = client_socket.recv(1024).decode('utf-8')
+response = client_socket.recv(1024).decode('utf-8')#fetch the repsonse message from server
 print(response)
 
-if "Granted" in response:
+if "Granted" in response: #correct password connection allowed
     print("connected")
-    prompt_data_input = client_socket.recv(1024).decode('utf-8')
+    prompt_data_input = client_socket.recv(1024).decode('utf-8')#fetch data input prompt message from server
     print(prompt_data_input)
     while True:
-        data +=input("")
+        data +=input("") #append data unitl 'end'
         if "end" in data:
-            client_socket.sendall(data.encode('utf-8'))
+            client_socket.sendall(data.encode('utf-8'))#send data to server
             break
 
-    confirmation_msg = client_socket.recv(1024).decode('utf-8')
+    confirmation_msg = client_socket.recv(1024).decode('utf-8')#fetch confirmation message from server
     print(confirmation_msg)
         
 
-else:
+else: #incorrect password connection not allowed
     print("not connected")
 
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket 
 
-host = '0.0.0.0'         #default host 
+host = '0.0.0.0'   #default host accept all interfaces 
 data = ""
-port = 12800                                              #using free port 5000
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)           #setup TCP socket 
-server_socket.bind((host,port))                            #Bind the socket to the host and port
+port = 12800      #using free port 12800
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)          #setup TCP socket 
+server_socket.bind((host,port))  #Bind the socket to the host and port
 server_socket.listen()  # The server starts listening for client connections
 print(f"server socket start listening at host {host} port {port}")
 
@@ -12,31 +12,31 @@ while True:
     client_socket , client_address = server_socket.accept() #waiting for client connection
     print(f"Client {client_address} is trying to connect")
 
-    password_prompt = ("Input Server Password : ") #prompt client to input password
-    client_socket.sendall(password_prompt.encode('utf-8'))
+    password_prompt = ("Input Server Password : ") 
+    client_socket.sendall(password_prompt.encode('utf-8')) #send password prompt to client socket 
 
-    user_input_code = client_socket.recv(1024).decode('utf-8')
+    user_input_code = client_socket.recv(1024).decode('utf-8') #fetch inputed password by client
     
 
     if user_input_code == '1234':
         print(f"Correct password connection to {client_address} successfull")
-        client_socket.sendall("Acess Granted".encode('utf-8'))
-        #if client logged in correctly start receiving message until keyword end
+        client_socket.sendall("Acess Granted".encode('utf-8')) #send message to client socket
        
-        client_socket.sendall("Input Data (To quit type 'end')".encode('utf-8'))
-        while True:
-            data = client_socket.recv(1024).decode('utf-8')  # Receive data from the client
-            if data == "":
-                print(f"Connection with {client_address} has been closed, message not received")
-                break
-            elif "end" in data: 
-                print(f"message received {data}")
-                client_socket.sendall("Messaged was received successfully".encode('utf-8'))
-                break
+        client_socket.sendall("Input Data (To quit type 'end')".encode('utf-8'))#send prompt to input data to client socket
+        
+        data = client_socket.recv(1024).decode('utf-8')  # fetch data from the client
+        if data == "":
+            print(f"Connection with {client_address} has been closed, message not received")
+            
+        elif "end" in data: 
+            print(f"message received {data}")
+            client_socket.sendall("Messaged was received successfully".encode('utf-8'))#send message to client socket
+                
 
-        client_socket.close()
+        client_socket.close() #close client socket
+    
     else:
-        print(f"Error Incorrect Password connection denied to {client_address}")
-        client_socket.sendall("Acess Denied".encode('utf-8'))
-        client_socket.close()
+        print(f"Error Incorrect Password connection denied to {client_address}") 
+        client_socket.sendall("Acess Denied".encode('utf-8')) #send message to client socket
+        client_socket.close() #close client socket
 
